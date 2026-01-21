3732b4f89b229d54f5119246d4212e4a34a04e16
FrankC4 <frankC4.github@gmail.com>
2026-01-21 20:27:35 +0100

    now it should work

--- fancyRPM.h
+++ fancyRPM.h
@@ -1,8 +1,1 @@
-// see src header
-
-#ifndef FANCY_RPM_H
-    #define FANCY_RPM_H
-
-    #include "include/fancyRPM.h"
-
-#endif
+#include "include/fancyRPM.h"
--- include/fancyRPM.h
+++ include/fancyRPM.h
@@ -1,6 +1,1 @@
-#ifndef FCK4_FANCY_RPM_H
-    #define FCK4_FANCY_RPM_H
-
-    #include "../src/fkc4/fancy_rpm/fancyRPM.h"
-
-#endif
+#include "../src/fkc4/fancy_rpm/fancyRPM.h"
